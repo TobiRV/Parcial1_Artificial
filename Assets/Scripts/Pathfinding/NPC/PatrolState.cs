d309eb24653a9aeeb803bbb3ee7f6eccc0047bc5
G1tCh4rl3s <166022559+G1tCh4rl3s@users.noreply.github.com>
2024-11-04 12:29:10 -0300

    drger

--- Assets/Scripts/Pathfinding/NPC/PatrolState.cs
+++ Assets/Scripts/Pathfinding/NPC/PatrolState.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +5,7 @@ using UnityEngine;
 public class PatrolState : IState
 {
     private NPCController npc;
-    public static Pathfinding Instance { get; private set; }
 
-    public LayerMask nodeMask;
-
-    private Node fromNode;
-    private Node toNode;
-
-    private HashSet<Node> closeNodes;
-    private PriorityQueue<Node> openNodes;
-    private struct PathRequestData
-    {
-        public Vector3 fromPoint;
-        public Vector3 toPoint;
-        public Action<List<Node>> callbackPath;
-    }
-
-    private Queue<PathRequestData> queuePath = new Queue<PathRequestData>();
-    private bool isCalculating = false;
-
-    private Action OnResetNodes = delegate { };
-
-    [SerializeField] private float yMargin;
 
     public PatrolState(NPCController npc)
     {
@@ -37,19 +15,12 @@ public class PatrolState : IState
     public void Enter()
     {
         // Agregar inicio de patrullaje con primer nodo
-        Instance = this;
         Debug.Log("Entering Patrol State");
     }
 
     public void Update()
     {
-        if (!(queuePath.Count > 0) || isCalculating) return;
-
-        OnResetNodes();
-        OnResetNodes = delegate () { };
-        isCalculating = true;
-        var actualData = queuePath.Dequeue();
-        StartCoroutine(Path(actualData.fromPoint, actualData.toPoint, actualData.callbackPath));
+        
     }
 
     public void Exit()
